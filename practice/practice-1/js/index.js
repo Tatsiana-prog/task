6f65fe3815ba36b85b047f5c6533a661c624f226
tatsiana-prog <programvtv07@gmail.com>
2023-12-25 19:53:17 +0300

    fix:changed code for logo-image

--- practice/practice-1/js/index.js
+++ practice/practice-1/js/index.js
@@ -1,18 +1,17 @@
-const imageContainer = document.getElementById('image-container');
 const fileInput = document.getElementById('file-input');
-
-imageContainer.addEventListener('click', function() {
-  fileInput.click();
-});
+const imagePreview = document.getElementById('image-preview');
 
 fileInput.addEventListener('change', function() {
   const file = fileInput.files[0];
   const reader = new FileReader();
 
   reader.onload = function(e) {
-    const image = document.getElementById('image');
-    image.src = e.target.result;
+    imagePreview.src = e.target.result;
   };
 
   reader.readAsDataURL(file);
+});
+
+imagePreview.addEventListener('click', function() {
+  fileInput.click();
 });
